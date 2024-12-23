951361d9c91cf5766859da1c070e2b83f7a714d5
ade <fajobiadedoyin@hotmail.com>
2024-12-23 11:41:56 +0100

    Update axios.ts

--- client/src/lib/axios.ts
+++ client/src/lib/axios.ts
@@ -4,6 +4,6 @@ export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
       ? "http://localhost:4009/api"
-      : "/api ",
+      : "/api",
   withCredentials: true
 });
